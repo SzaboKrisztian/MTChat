30233a30ad86e0c59199d3a083f8a2c4e392cc9f
Chris Szabo <szabo.krisztian.87@gmail.com>
2019-09-20 08:51:29 +0200

    Initial commit

---
+++ MTChatServer/src/ClientHandler.java
@@ -0,0 +1,17 @@
+public class ClientHandler implements Observer, Runnable {
+  private MessageServer messageServer;
+
+  public ClientHandler(MessageServer messageServer) {
+    this.messageServer = messageServer;
+  }
+
+  @Override
+  public void update() {
+
+  }
+
+  @Override
+  public void run() {
+
+  }
+}
---
+++ MTChatServer/src/MessageServer.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageServer implements Observable {
+  private List<Observer> observers;
+
+  public MessageServer() {
+    observers = new ArrayList<>();
+  }
+
+  @Override
+  public void addObserver(Observer observer) {
+    observers.add(observer);
+  }
+
+  @Override
+  public void removeObserver(Observer observer) {
+    observers.remove(observer);
+  }
+
+  @Override
+  public void notifyObservers() {
+    for (Observer observer: observers) {
+      observer.update();
+    }
+  }
+}
